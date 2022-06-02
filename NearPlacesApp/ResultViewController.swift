c02d00df88b72cf0d1769e9ff5d51300fadd7326
Atahan Sezgin <atahan.sezgin@bulutmd.com>
2022-06-02 23:43:49 +0300

    initial commit

---
+++ NearPlacesApp/ResultViewController.swift
@@ -0,0 +1,72 @@
+//
+//  ResultViewController.swift
+//  NearPlacesApp
+//
+//  Created by Atahan Sezgin on 1.06.2022.
+//
+
+import UIKit
+import CoreLocation
+
+protocol ResultViewControllerDelegate : AnyObject {
+    func didTapPlace(with coordinates:CLLocationCoordinate2D)
+}
+
+class ResultViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    weak var delegate : ResultViewControllerDelegate?
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self,
+                    forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
+    private var places:[Place]=[]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(tableView)
+        view.backgroundColor = .clear
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+    
+    public func update(with places: [Place]){
+        self.tableView.isHidden = false
+        self.places = places
+        print(places.count)
+        tableView.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = places[indexPath.row].name
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        tableView.isHidden = true
+        let place = places[indexPath.row]
+        GooglePlacesManager.shared.resolveLocation(for: place){ [weak self] result in
+            switch result {
+            case .success(let coordinate):
+                DispatchQueue.main.async {
+                    self?.delegate?.didTapPlace(with: coordinate)
+                }
+            case .failure(let error):
+            print(error)
+            }
+        }
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section:Int) -> Int {
+        return places.count
+    }
+
+}
--- NearPlacesApp/ViewController.swift
+++ NearPlacesApp/ViewController.swift
@@ -6,14 +6,80 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UISearchResultsUpdating {
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let query = searchController.searchBar.text,
+              !query.trimmingCharacters(in: .whitespaces).isEmpty,
+        let resultsVC = searchController.searchResultsController as? ResultViewController else {
+            return
+        }
+        
+        resultsVC.delegate = self
+        
+        GooglePlacesManager.shared.findPlaces(query: query){ result in
+            switch result {
+            case .success(let places):
+                DispatchQueue.main.async {
+                    resultsVC.update(with:places)
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    let mapView = MKMapView()
+    
+    let searchViewController = UISearchController(
+        searchResultsController: ResultViewController()
+    )
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "Haritalar"
+        view.addSubview(mapView)
+        searchViewController.searchBar.placeholder = "Ara"
+        searchViewController.searchResultsUpdater = self
+        navigationItem.searchController = searchViewController
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        mapView.frame = CGRect(
+            x: 0,
+            y: view.safeAreaInsets.top,
+            width: view.frame.size.width,
+            height: view.frame.size.height
+        )
     }
 
 
 }
 
+extension ViewController: ResultViewControllerDelegate {
+    func didTapPlace(with coordinates: CLLocationCoordinate2D) {
+        searchViewController.searchBar.resignFirstResponder()
+        searchViewController.dismiss(animated: true,completion: nil)
+        // Remove all map pins
+        let annotations = mapView.annotations
+        mapView.removeAnnotations(annotations)
+        
+        // Add a map pin
+        let pin = MKPointAnnotation()
+        pin.coordinate = coordinates
+        mapView.addAnnotation(pin)
+        mapView.setRegion(
+            MKCoordinateRegion(
+                center: coordinates,
+                span: MKCoordinateSpan(
+                    latitudeDelta: 0.2,
+                    longitudeDelta: 0.2
+                )),
+            animated: true)
+    }
+    
+    
+}
